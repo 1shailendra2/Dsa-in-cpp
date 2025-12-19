1462710353fb86bd46ca21552bad0201a7fe75bb
1shailendra2 <bhandarishailesh81@gmail.com>
2025-12-19 15:45:11 +0545

    added another one

---
+++ patterns/invertedv.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+int main(){
+using namespace std;
+int n;
+cin>>n;
+for(int i=0; i<n; i++){
+	for(int j=0;j<n-i-1;j++){
+         cout<<" ";
+	}
+	for(int j=0; j<2*i+1;j++){
+		if(j==0){
+			cout<<"*";
+		}
+		else if(j== 2*i){
+cout<<"*";
+		}
+		else{
+			cout<<" ";
+		}
+
+	
+	}
+	cout<<endl;
+}}
